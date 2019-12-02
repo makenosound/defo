a0ed37e9eda5010562dfde6398ca79a47d4b88e1
Max Wheeler <max@makenosound.com>
2019-12-02 15:34:13 +1100

    Remove register/unregister view functions for now

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,6 @@
 import { Views } from "./types";
 import observe from "./observe";
 
-function registerViews(currentViews: Views, viewsToAdd: Views) {
-  return {
-    ...currentViews,
-    ...viewsToAdd
-  };
-}
-
-function unregisterViews(currentViews: Views, viewsToRemove: Array<string>) {
-  const updatedViews: Views = {};
-  Object.keys(currentViews).forEach((key: string) => {
-    if (viewsToRemove.indexOf(key) === -1) {
-      updatedViews[key] = currentViews[key];
-    }
-  });
-  return updatedViews;
-}
-
 export default function defo({
   prefix = "defo",
   scope = document.documentElement,
@@ -30,13 +13,6 @@ export default function defo({
   const observer = observe({prefix, scope, views});
 
   return {
-    registerViews: (viewsToAdd: Views) => {
-      views = registerViews(views, viewsToAdd);
-      return views;
-    },
-    unregisterViews: (viewsToRemove: Array<string>) => {
-      views = unregisterViews(views, viewsToRemove);
-      return views;
     }
     // render
   };
